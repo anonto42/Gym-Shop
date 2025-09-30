292d61de5a06488ae26a4568a3f4bfb61769d8a0
Sohidul Islam Ananto <anontom90@gmail.com>
2025-09-30 22:34:56 +0600

    starter modules added

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, ProductModule, AdminModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
