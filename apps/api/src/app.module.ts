c3cf9ea88c366f6405459457b6e339fab9692880
Sohidul Islam Ananto <anontom90@gmail.com>
2025-10-01 23:14:59 +0600

    services defined witht the moduels

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,9 +6,13 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
 import { AdminModule } from './admin/admin.module';
+import { OrderModule } from './order/order.module';
+import { PaymentsModule } from './payments/payments.module';
+import { FilesModule } from './files/files.module';
+import { JobsModule } from './jobs/jobs.module';
 
 @Module({
-  imports: [AuthModule, UserModule, ProductModule, AdminModule],
+  imports: [AuthModule, UserModule, ProductModule, AdminModule, OrderModule, PaymentsModule, FilesModule, JobsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
