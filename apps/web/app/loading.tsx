7c399617a58dbd234b538930fa97ace732521b3c
Sohidul Islam Ananto <anontom90@gmail.com>
2025-10-07 06:53:40 +0600

    removeed

--- apps/web/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-export default function Loading() {
-    return (
-      <div className="fixed inset-0 flex items-center justify-center bg-white/70 backdrop-blur-sm z-[9999]">
-        <div className="w-12 h-12 border-4 border-t-[#00AEFF] border-gray-300 rounded-full animate-spin"></div>
-        <p className="text-[#00AEFF] font-medium mt-4">Loading...</p>
-      </div>
-    );
-  }
-  
