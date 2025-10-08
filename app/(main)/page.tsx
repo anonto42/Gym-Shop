8bcfa5a30736f054a685ea83ca48b1b11755c696
Sohidul Islam Ananto <anontom90@gmail.com>
2025-10-08 22:44:35 +0600

    added the new section

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,12 +1,14 @@
 import AutoSlider from "@/components/section/AutoSlider";
 import HeroSection from "@/components/section/HeroSection";
+import ValuablePackages from "@/components/section/ValuablePackages";
 
 
 export default function Home() {
   return (
-    <div className="w-full h-svh">
+    <div className="w-full h-auto">
       <HeroSection />
       <AutoSlider />
+      <ValuablePackages />
     </div>
   );
 }
