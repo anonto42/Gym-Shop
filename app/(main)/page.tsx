c215696f123c9d0d0dacf5184b34acf2636b754d
Sohidul Islam Ananto <anontom90@gmail.com>
2025-10-08 07:46:19 +0600

    added 2 section

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,12 @@
+import AutoSlider from "@/components/section/AutoSlider";
+import HeroSection from "@/components/section/HeroSection";
 
 
 export default function Home() {
   return (
     <div className="w-full h-svh">
-
+      <HeroSection />
+      <AutoSlider />
     </div>
   );
 }
