22506946f5f3a8aff07444f94bc0c014ce5f3350
Sohidul Islam Ananto <anontom90@gmail.com>
2025-10-09 22:37:43 +0600

    added max w

--- components/layout/TopBar.tsx
+++ components/layout/TopBar.tsx
@@ -5,30 +5,32 @@ import { UserCircle } from "lucide-react";
 
 const TopBar = () => {
   return (
-    <div className="w-full bg-[#222222] text-white sm:text-xs text-[7px] py-2 px-4 flex justify-end items-center">
-      {/* Left links */}
-      <div className="flex gap-4">
-        <Link href="/contact-us" className="hover:underline border-r pr-3">
-          Contact Us
-        </Link>
-        <Link href="/privacy-policy" className="hover:underline border-r pr-3">
-          Privacy & Return Policy
-        </Link>
-        <Link href="/track-order" className="hover:underline border-r pr-3 mr-3">
-          Track Order
-        </Link>
-      </div>
+    <div className="w-full bg-[#222222]">
+      <div className=" text-white sm:text-xs text-[7px] py-2 px-4 flex justify-end items-center max-w-[1540px] mx-auto">
+        {/* Left links */}
+        <div className="flex gap-4">
+          <Link href="/contact-us" className="hover:underline border-r pr-3">
+            Contact Us
+          </Link>
+          <Link href="/privacy-policy" className="hover:underline border-r pr-3">
+            Privacy & Return Policy
+          </Link>
+          <Link href="/track-order" className="hover:underline border-r pr-3 mr-3">
+            Track Order
+          </Link>
+        </div>
 
-      {/* Right links */}
-      <div className="flex gap-3">
-        <UserCircle size={16} />
-        <Link href="/auth/signin" className="hover:underline">
-          Sign In
-        </Link>
-        <span>/</span>
-        <Link href="/auth/signup" className="hover:underline">
-          Sign Up
-        </Link>
+        {/* Right links */}
+        <div className="flex gap-3">
+          <UserCircle size={16} />
+          <Link href="/auth/signin" className="hover:underline">
+            Sign In
+          </Link>
+          <span>/</span>
+          <Link href="/auth/signup" className="hover:underline">
+            Sign Up
+          </Link>
+        </div>
       </div>
     </div>
   );
