07e0a9e556fff46fadebeb21e708156f0987435a
Sohidul Islam Ananto <anontom90@gmail.com>
2025-10-25 15:16:25 +0600

    Sign Up function created successfully

--- server/functions/auth.fun.ts
+++ server/functions/auth.fun.ts
@@ -8,20 +8,32 @@ import {SendResponse} from "@/server/helper/sendResponse.helper";
 import {sendEmail} from "@/server/helper/emailSender.helper";
 import {GenerateCreateAccountHtml} from "@/enum/htmlCreate.helper";
 import {generateOTP} from "@/server/helper/generateOtp.helper";
+import connectToDB from "@/server/db";
+import {handleServerError} from "@/server/helper/ErrorHandler";
+import {ServerError} from "@/server/interface/serverError.interface";
+
+await connectToDB();
 
 export async function signUpServerSide ( body: ISignUpInput ): Promise<IUser | IResponse> {
+    try {
+        const name = body.get("name") as string;
+        const email = body.get("email") as string;
+        const password = body.get("password") as string;
 
-    const name = body.get("name") as string;
-    const email = body.get("email") as string;
-    const password = body.get("password") as string;
+        if ( await UserModel.exists({ email })) return SendResponse({ isError: true, status: 409, message: "You email was already exist!" })
 
-    if ( await UserModel.exists({ email })) return SendResponse({ isError: true, status: 409, message: "You email was already exist!" })
+        // Create html
+        const html = GenerateCreateAccountHtml( generateOTP(6) );
 
-    // Create html
-    const html = GenerateCreateAccountHtml( generateOTP(6) );
+        // Send Otp for users
+        await sendEmail({ to:email, html, subject:`Verify you ${process.env.COMPANY_NAME} account`});
 
-    // Send Otp for users
-    await sendEmail({ to:email, html, subject:`Verify you ${process.env.COMPANY_NAME} account`});
+        const newUser = await UserModel.create({name, email, password});
 
-    return await UserModel.create({name, email, password});
+        console.log("New user created", newUser);
+
+        return  newUser;
+    } catch (error : ServerError ) {
+        return handleServerError(error);
+    }
 }
