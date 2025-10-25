2261aad92ea1e64a77efe904130fae415602ef10
Sohidul Islam Ananto <anontom90@gmail.com>
2025-10-25 22:13:29 +0600

    Sign in page added

--- server/functions/auth.fun.ts
+++ server/functions/auth.fun.ts
@@ -2,7 +2,7 @@
 
 import {UserModel} from "@/server/models/user/user.model";
 import {IUser} from "@/server/models/user/user.interfce";
-import {ISignUpInput} from "@/server/interface/auth.interface";
+import {ISignInInput, ISignUpInput} from "@/server/interface/auth.interface";
 import {IResponse} from "@/server/interface/response.interface";
 import {SendResponse} from "@/server/helper/sendResponse.helper";
 import {sendEmail} from "@/server/helper/emailSender.helper";
@@ -11,6 +11,7 @@ import {generateOTP} from "@/server/helper/generateOtp.helper";
 import connectToDB from "@/server/db";
 import {handleServerError} from "@/server/helper/ErrorHandler";
 import {ServerError} from "@/server/interface/serverError.interface";
+import { sign } from 'jsonwebtoken';
 
 await connectToDB();
 
@@ -22,17 +23,46 @@ export async function signUpServerSide ( body: ISignUpInput ): Promise<IUser | I
 
         if ( await UserModel.exists({ email })) return SendResponse({ isError: true, status: 409, message: "You email was already exist!" })
 
+        // Create OTP
+        const otp = generateOTP(6);
+
         // Create html
-        const html = GenerateCreateAccountHtml( generateOTP(6) );
+        const html = GenerateCreateAccountHtml( otp );
 
         // Send Otp for users
         await sendEmail({ to:email, html, subject:`Verify you ${process.env.COMPANY_NAME} account`});
 
-        const newUser = await UserModel.create({name, email, password});
-
-        console.log("New user created", newUser);
+        const newUser = await UserModel.create({name, email, password, otp});
 
         return  newUser;
+    } catch (error : ServerError ) {
+        return handleServerError(error);
+    }
+}
+
+export async function signInServerSide ( body: ISignInInput ): Promise<string | IResponse > {
+    try {
+        const email = body.get("email") as string;
+        const password = body.get("password") as string;
+
+        const user  = await UserModel.findUserByEmail(email);
+        if ( !user ) return SendResponse({ isError: true, status: 404, message: "Email was not exist!" });
+
+        const response: boolean | { isError: boolean, status: number, message: string } = await UserModel.isPasswordMac(email,password);
+        if( typeof response != "boolean" && response.isError == true ){
+            return SendResponse({
+                isError: response.isError,
+                status: response.status,
+                message: response.message
+            });
+        }
+
+        return sign({
+            email,
+            id: user._id,
+            role: user.role
+        }, process.env.JWT_EXPIRY as string);
+
     } catch (error : ServerError ) {
         return handleServerError(error);
     }
