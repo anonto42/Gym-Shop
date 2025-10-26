56b1c4a5c0aac5803eb1acd695c08ac572eaea09
Sohidul Islam Ananto <anontom90@gmail.com>
2025-10-26 18:53:53 +0600

    Remort origin added

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["res.cloudinary.com"],
+      domains: ["res.cloudinary.com"],
+      // Or use remotePatterns (recommended):
+      remotePatterns: [
+          {
+              protocol: 'https',
+              hostname: 'res.cloudinary.com',
+          },
+      ],
   },
 };
 
