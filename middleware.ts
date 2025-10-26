43ff17e0b7e73ba4688a6c2a9b06e39c138ed172
Sohidul Islam Ananto <anontom90@gmail.com>
2025-10-26 23:47:39 +0600

    middleware created for the protected routes

---
+++ middleware.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import {getCookie} from "@/server/helper/jwt.helper";
+import {IUser} from "@/server/models/user/user.interfce";
+import {USER_ROLE} from "@/enum/user.enum";
+
+export async function middleware(request: NextRequest) {
+    // Define your protected routes
+    const protectedRoutesForAdmins = ['/dashboard'];
+    const protectedRoutes = ['/profile'];
+
+    const path = request.nextUrl.pathname;
+
+    // Skip middleware for internal Next.js paths (e.g., _next)
+    if (path.startsWith('/_next')) {
+        return NextResponse.next();
+    }
+
+    const isAuthenticated = await getCookie("user");
+
+    if (typeof isAuthenticated !== "string" && protectedRoutes.includes(path)) {
+        const loginUrl = new URL('/auth/signin', request.url);
+        return NextResponse.redirect(loginUrl);
+    }
+
+    const user = JSON.parse(isAuthenticated as string) as IUser;
+
+    if (user.role != USER_ROLE.ADMIN && protectedRoutesForAdmins.includes(path)) {
+        const returnUrl = new URL('/', request.url);
+        return NextResponse.redirect(returnUrl);
+    }
+
+    return NextResponse.next();
+}
