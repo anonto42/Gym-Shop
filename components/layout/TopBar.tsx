736130e5abd8076f486f264cb0faa022d1ced34b
Sohidul Islam Ananto <anontom90@gmail.com>
2025-10-26 23:25:58 +0600

    added the data featching

--- components/layout/TopBar.tsx
+++ components/layout/TopBar.tsx
@@ -1,9 +1,34 @@
 "use client";
-import React from "react";
+
+import React, {useEffect, useState} from "react";
 import Link from "next/link";
 import { UserCircle } from "lucide-react";
+import {isAuthenticatedAndGetUser} from "@/server/functions/auth.fun";
+import {IUser} from "@/server/models/user/user.interfce";
+import {getCookie, setCookie} from "@/server/helper/jwt.helper";
 
 const TopBar = () => {
+    const [user, setUser] = useState<IUser | null>(null);
+
+    useEffect(() => {
+        ;( async ()=> {
+            const cookie = await getCookie("user");
+            if (typeof cookie == 'string' ) return setUser( JSON.parse(cookie) );
+            else {
+                const res = await isAuthenticatedAndGetUser();
+                if ( typeof res != "string" && res.isError == true) {
+                    console.log(res);
+                    setUser(null)
+                    return;
+                } else if ( typeof res == "string" ) {
+                    await setCookie({name:"user", value: res });
+                    setUser(JSON.parse(res));
+                    return;
+                }
+            }
+        })()
+    }, []);
+
   return (
     <div className="w-full bg-[#222222]">
       <div className=" text-white sm:text-xs text-[7px] py-2 px-4 flex justify-end items-center max-w-[1540px] mx-auto">
@@ -19,14 +44,25 @@ const TopBar = () => {
 
         {/* Right links */}
         <div className="flex gap-3">
-          <UserCircle size={16} />
-          <Link href="/auth/signin" className="hover:underline">
-            Sign In
-          </Link>
-          <span>/</span>
-          <Link href="/auth/signup" className="hover:underline">
-            Sign Up
-          </Link>
+            {
+                user?
+                    (
+                        <Link href="/profile" className="hover:underline" title={"Profile"}>
+                            <UserCircle size={16} />
+                        </Link>
+                    )   :
+                    (
+                        <>
+                          <Link href="/auth/signin" className="hover:underline">
+                            Sign In
+                          </Link>
+                          <span>/</span>
+                          <Link href="/auth/signup" className="hover:underline">
+                            Sign Up
+                          </Link>
+                        </>
+                    )
+            }
         </div>
       </div>
     </div>
