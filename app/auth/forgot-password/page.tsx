28d312a289efd4e234a7a6ef12338c56e4da8b29
Sohidul Islam Ananto <anontom90@gmail.com>
2025-10-26 10:16:58 +0600

    added the dinamic

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -1,28 +1,56 @@
 "use client";
+
 import React, { useState } from "react";
 import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import {IError} from "@/server/interface/error.interface";
+import {toast} from "sonner";
+import Loader from "@/components/loader/Loader";
+import {forgotPasswordServerSide} from "@/server/functions/auth.fun";
+import {IForgotPasswordInput} from "@/server/interface/auth.interface";
 
 const ForgotPassword = () => {
   const router = useRouter();
-  const [email, setEmail] = useState("");
+  const [error, setError] = useState<IError>({field: null, message: null});
+  const [email, setEmail] =useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
 
-  const handleResetRequest = () => {
+  const handleResetRequest = async () => {
+    setEmail("");
+    setLoading(true);
+    setError({field: null, message: null});
+
     if (!email) {
-      alert("Please enter your email address");
+      setError({field: "email", message: "Email is required"});
+      setLoading(false);
       return;
     }
 
-    // ✅ Here you can add your backend API call to send OTP or reset link
-    console.log("Reset link sent to:", email);
+    const formData = new FormData() as FormData & IForgotPasswordInput;
+    formData.append("email", email);
 
-    // After sending OTP or reset link, redirect to OTP verify page
-    router.push("/auth/verify-otp");
+    const response = await forgotPasswordServerSide(formData);
+
+    if (response.isError) {
+        toast.error(response.message);
+        setLoading(false);
+        return null;
+    }
+
+    setLoading(false);
+
+    toast.success('OTP send on email!');
+
+    setTimeout(() => {
+        router.push("/auth/verify-otp");
+    }, 800);
+
   };
 
   return (
     <div className="flex items-center justify-center h-screen shadow-xl shadow-black">
+        { loading && <Loader size={"lg"} overlay={true} message={"Loading..."} /> }
       <Card className="w-[350px] h-auto border border-white backdrop-blur-3xl bg-white/20">
         <CardHeader className="text-center">
           <CardTitle className="text-2xl font-bold text-white">
@@ -42,20 +70,21 @@ const ForgotPassword = () => {
             onChange={(e) => setEmail(e.target.value)}
             className="w-full p-2 border border-white rounded-md focus:outline-amber-200 focus:outline-1 placeholder:text-white placeholder:font-light text-white"
           />
+            { error.field == "email" && (<span className={"text-red-500 text-xs font-semibold"}>{error.message}</span>)}
         </CardContent>
 
         <CardFooter className="gap-3 grid">
           <Button
             variant="outline"
-            className="w-full bg-black text-white border-0 text-xs"
+            className="w-full bg-black text-white border-0 text-xs cursor-pointer"
             onClick={handleResetRequest}
           >
-            Send Reset Link
+            Send Reset OTP
           </Button>
 
           <Button
             variant="outline"
-            className="w-full bg-white text-black border-0 text-xs"
+            className="w-full bg-white text-black border-0 text-xs cursor-pointer"
             onClick={() => router.push("/auth/signin")}
           >
             Back to Login
