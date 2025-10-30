767f37644396e6815f183e202ede01008333f4cd
Sohidul Islam Ananto <anontom90@gmail.com>
2025-10-31 01:02:50 +0600

    creating the page

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -1,33 +1,80 @@
 "use client";
-import React, { useState } from "react";
+
+import React, {useEffect, useState} from "react";
 import Image from "next/image";
-import imageUrl from "@/const/imageUrl";
+import {getCookie, setCookie} from "@/server/helper/jwt.helper";
+import {isAuthenticatedAndGetUser} from "@/server/functions/auth.fun";
+import {IUser} from "@/server/models/user/user.interfce";
+import {USER_ROLE, USER_STATUS} from "@/enum/user.enum";
+import {SquarePen} from "lucide-react";
+import {toast} from "sonner";
 
 function ProfilePage() {
-  const [user] = useState({
-    name: "John Doe",
-    email: "johndoe@email.com",
-    image: imageUrl.product,
-    joined: "March 2024",
+  const [user,setUser] = useState<IUser>({
+      name: "",
+      image: "https://res.cloudinary.com/ddsnont4o/image/upload/v1760025348/jhkjjhk_iesrwh.png",
+      email: "",
+      password: "",
+      status: USER_STATUS.ACTIVE,
+      isVerified: true,
+      role: USER_ROLE.USER,
+      createdAt: new Date()
   });
+  const [edite, setEdite] = useState<boolean>(false);
 
   const [orders] = useState([
     { id: "12345", product: "Smart Lamp", date: "Sep 5, 2025", status: "Delivered" },
     { id: "67890", product: "Air Purifier Pro", date: "Sep 12, 2025", status: "Shipped" },
   ]);
 
-  return (
+  useEffect(() => {
+    toast.info(`You current editing state on ${edite}`);
+  }, [edite]);
+
+  useEffect(() => {
+      ;( async ()=> {
+          const cookie = await getCookie("user");
+          if (typeof cookie == 'string' ) return setUser( JSON.parse(cookie) );
+          else {
+              const res = await isAuthenticatedAndGetUser();
+              if ( typeof res != "string" && res.isError == true) {
+                  setUser({
+                      name: "string",
+                      image: "https://res.cloudinary.com/ddsnont4o/image/upload/v1760025348/jhkjjhk_iesrwh.png",
+                      email: "string",
+                      password: "string",
+                      status: USER_STATUS.ACTIVE,
+                      isVerified: true,
+                      role: USER_ROLE.USER,
+                      createdAt: new Date()
+                  })
+                  return;
+              } else if ( typeof res == "string" ) {
+                  await setCookie({name:"user", value: res });
+                  setUser(JSON.parse(res));
+                  return;
+              }
+          }
+        })()
+    }, []);
+
+    return (
     <section className="w-full min-h-screen bg-white py-16 px-6 md:px-12 lg:px-20">
       <div className="max-w-5xl mx-auto">
         {/* Header */}
         <div className="flex flex-col md:flex-row items-center gap-6 mb-10">
           <div className="relative w-32 h-32 rounded-full overflow-hidden border-4 border-[#F27D31]">
             <Image src={user.image} alt="profile" fill className="object-cover" />
+            <div className={"absolute w-[40px] h-[40px] bottom-2 right-2 bg-[#232323]/20 text-white z-50 cursor-pointer rounded-full"}>
+                <div onClick={ () => setEdite( !edite ) } className={"w-full h-full relative flex items-center justify-center"}>
+                    <SquarePen className={"text-black"} />
+                </div>
+            </div>
           </div>
           <div>
             <h1 className="text-3xl font-bold text-gray-900">{user.name}</h1>
             <p className="text-gray-500">{user.email}</p>
-            <p className="text-gray-500 text-sm mt-2">Joined: {user.joined}</p>
+            <p className="text-gray-500 text-sm mt-2">Joined: {new Date(user.createdAt!).toDateString()}</p>
           </div>
         </div>
 
