72c73f1d716842f2ea4ff5ba4aa2d330db56669f
Sohidul Islam Ananto <anontom90@gmail.com>
2025-11-05 21:53:08 +0600

    used the lazy import on the Dashboard layout

--- components/layout/DashboardLayout.tsx
+++ components/layout/DashboardLayout.tsx
@@ -1,18 +1,20 @@
 "use client";
-import {useState} from "react";
+import { lazy, useState } from "react";
 import Navbar from "../bar/Navbar";
 import Sidebar from "../bar/Sidebar";
-import OverviewTab from "../tabs/Overview";
-import Hero from "../tabs/Hero";
-import Offer from "../tabs/Offer";
-import Products from "../tabs/Products";
-import PersonalTraining from "./PersonalTraining";
-import PackageManagement from "../tabs/PackageManagement";
-import AboutMe from "../tabs/AboutMe";
-import OurTeam from "../tabs/OurTeam";
-import UserTab from "../tabs/User";
-import PrivacyPolicy from "../tabs/PrivacyPolicy";
-import Contact from "../tabs/Contact";
+
+// Lazy load the tabs
+const OverviewTab = lazy(() => import("../tabs/Overview"));
+const Hero = lazy(() => import("../tabs/Hero"));
+const Offer = lazy(() => import("../tabs/Offer"));
+const Products = lazy(() => import("../tabs/Products"));
+const PersonalTraining = lazy(() => import("../tabs/PersonalTraining"));
+const PackageManagement = lazy(() => import("../tabs/PackageManagement"));
+const AboutMe = lazy(() => import("../tabs/AboutMe"));
+const OurTeam = lazy(() => import("../tabs/OurTeam"));
+const UserTab = lazy(() => import("../tabs/User"));
+const PrivacyPolicy = lazy(() => import("../tabs/PrivacyPolicy"));
+const Contact = lazy(() => import("../tabs/Contact"));
 
 export default function DashboardLayout () {
 
@@ -25,18 +27,19 @@ export default function DashboardLayout () {
                 <Sidebar activeTab={activeTab} setActiveTab={setActiveTab}/>
                 <div className={"w-full h-full overflow-auto"}>
                     { 
-                    activeTab == "Overview" ? <OverviewTab /> :
-                    activeTab == "Hero" ? <Hero /> :
-                    activeTab == "Offer" ? <Offer /> :
-                    activeTab == "Products" ? <Products /> :
-                    activeTab == "PersonalTraining" ? <PersonalTraining /> :
-                    activeTab == "PackageManagement" ? <PackageManagement /> :
-                    activeTab == "AboutMe" ? <AboutMe /> :
-                    activeTab == "OurTeam" ? <OurTeam /> :
-                    activeTab == "Contact" ? <Contact /> :
-                    activeTab == "User" ? <UserTab /> :
-                    activeTab == "PrivacyPolicy" ? <PrivacyPolicy /> :
-                    <h1>Not Found</h1> }
+                        activeTab == "Overview" ? <OverviewTab /> :
+                        activeTab == "Hero" ? <Hero /> :
+                        activeTab == "Offer" ? <Offer /> :
+                        activeTab == "Products" ? <Products /> :
+                        activeTab == "Personal Training" ? <PersonalTraining /> :
+                        activeTab == "Package Management" ? <PackageManagement /> :
+                        activeTab == "AboutMe" ? <AboutMe /> :
+                        activeTab == "OurTeam" ? <OurTeam /> :
+                        activeTab == "Contact" ? <Contact /> :
+                        activeTab == "User" ? <UserTab /> :
+                        activeTab == "PrivacyPolicy" ? <PrivacyPolicy /> :
+                        <h1>Not Found</h1> 
+                    }
                 </div>
             </div>
         </main>
