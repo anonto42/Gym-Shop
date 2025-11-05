a98c668f000f0afd377b58a2b64f5a0d46454b3a
Sohidul Islam Ananto <anontom90@gmail.com>
2025-11-05 15:28:34 +0600

    admin layout

---
+++ app/(admin)/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { Toaster } from 'sonner';
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Gym Shop Dashboard",
+  description: "Gym Shop Dashboard",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <main className="bg-[#F27D31] w-full h-svh">
+          {children}
+        </main>
+        <Toaster
+            position="top-right"
+            expand={true}
+            richColors
+            closeButton
+        />
+      </body>
+    </html>
+  );
+}
