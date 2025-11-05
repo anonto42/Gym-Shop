3ccc415ceb8cb06c5c5a789c4e6b1dfe1d4c8d9b
Sohidul Islam Ananto <anontom90@gmail.com>
2025-11-05 21:36:21 +0600

    Product tab created

--- components/tabs/Products.tsx
+++ components/tabs/Products.tsx
@@ -1,8 +1,123 @@
 import React from 'react'
+import { Button } from '../ui/button'
 
 function Products() {
   return (
-    <div>Products</div>
+    <div className='w-full h-[88vh] p-4'>
+      <div className='w-full h-full bg-white border rounded-3xl overflow-hidden'>
+        <div className='w-full flex justify-between items-center p-4 shadow-sm'>
+          <h1 className='text-2xl font-semibold'>Product Management</h1>
+          <Button className='bg-[#125BAC] cursor-pointer'>Add Product</Button>
+        </div>
+        <div className='w-full h-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 justify-center overflow-auto py-4'>
+          <div className='w-[300px] h-[100px] border rounded-2xl text-center relative mx-auto'>
+            <h3 className='text-xl font-semibold'>Title</h3>
+            <p className='text-sm'>Description</p>
+            <text className='text-sm'>Price</text>
+            <Button className='absolute cursor-pointer top-2 right-2 bg-[#125BAC] text-white p-2 rounded-full'>Edit</Button>
+            <Button className='absolute cursor-pointer bottom-2 right-2 bg-[#ac1212] text-white p-2 rounded-full'>Delete</Button>
+          </div>
+          <div className='w-[300px] h-[100px] border rounded-2xl text-center relative mx-auto'>
+            <h3 className='text-xl font-semibold'>Title</h3>
+            <p className='text-sm'>Description</p>
+            <text className='text-sm'>Price</text>
+            <Button className='absolute cursor-pointer top-2 right-2 bg-[#125BAC] text-white p-2 rounded-full'>Edit</Button>
+            <Button className='absolute cursor-pointer bottom-2 right-2 bg-[#ac1212] text-white p-2 rounded-full'>Delete</Button>
+          </div>
+          <div className='w-[300px] h-[100px] border rounded-2xl text-center relative mx-auto'>
+            <h3 className='text-xl font-semibold'>Title</h3>
+            <p className='text-sm'>Description</p>
+            <text className='text-sm'>Price</text>
+            <Button className='absolute cursor-pointer top-2 right-2 bg-[#125BAC] text-white p-2 rounded-full'>Edit</Button>
+            <Button className='absolute cursor-pointer bottom-2 right-2 bg-[#ac1212] text-white p-2 rounded-full'>Delete</Button>
+          </div>
+          <div className='w-[300px] h-[100px] border rounded-2xl text-center relative mx-auto'>
+            <h3 className='text-xl font-semibold'>Title</h3>
+            <p className='text-sm'>Description</p>
+            <text className='text-sm'>Price</text>
+            <Button className='absolute cursor-pointer top-2 right-2 bg-[#125BAC] text-white p-2 rounded-full'>Edit</Button>
+            <Button className='absolute cursor-pointer bottom-2 right-2 bg-[#ac1212] text-white p-2 rounded-full'>Delete</Button>
+          </div>
+          <div className='w-[300px] h-[100px] border rounded-2xl text-center relative mx-auto'>
+            <h3 className='text-xl font-semibold'>Title</h3>
+            <p className='text-sm'>Description</p>
+            <text className='text-sm'>Price</text>
+            <Button className='absolute cursor-pointer top-2 right-2 bg-[#125BAC] text-white p-2 rounded-full'>Edit</Button>
+            <Button className='absolute cursor-pointer bottom-2 right-2 bg-[#ac1212] text-white p-2 rounded-full'>Delete</Button>
+          </div>
+          <div className='w-[300px] h-[100px] border rounded-2xl text-center relative mx-auto'>
+            <h3 className='text-xl font-semibold'>Title</h3>
+            <p className='text-sm'>Description</p>
+            <text className='text-sm'>Price</text>
+            <Button className='absolute cursor-pointer top-2 right-2 bg-[#125BAC] text-white p-2 rounded-full'>Edit</Button>
+            <Button className='absolute cursor-pointer bottom-2 right-2 bg-[#ac1212] text-white p-2 rounded-full'>Delete</Button>
+          </div>
+          <div className='w-[300px] h-[100px] border rounded-2xl text-center relative mx-auto'>
+            <h3 className='text-xl font-semibold'>Title</h3>
+            <p className='text-sm'>Description</p>
+            <text className='text-sm'>Price</text>
+            <Button className='absolute cursor-pointer top-2 right-2 bg-[#125BAC] text-white p-2 rounded-full'>Edit</Button>
+            <Button className='absolute cursor-pointer bottom-2 right-2 bg-[#ac1212] text-white p-2 rounded-full'>Delete</Button>
+          </div>
+          <div className='w-[300px] h-[100px] border rounded-2xl text-center relative mx-auto'>
+            <h3 className='text-xl font-semibold'>Title</h3>
+            <p className='text-sm'>Description</p>
+            <text className='text-sm'>Price</text>
+            <Button className='absolute cursor-pointer top-2 right-2 bg-[#125BAC] text-white p-2 rounded-full'>Edit</Button>
+            <Button className='absolute cursor-pointer bottom-2 right-2 bg-[#ac1212] text-white p-2 rounded-full'>Delete</Button>
+          </div>
+          <div className='w-[300px] h-[100px] border rounded-2xl text-center relative mx-auto'>
+            <h3 className='text-xl font-semibold'>Title</h3>
+            <p className='text-sm'>Description</p>
+            <text className='text-sm'>Price</text>
+            <Button className='absolute cursor-pointer top-2 right-2 bg-[#125BAC] text-white p-2 rounded-full'>Edit</Button>
+            <Button className='absolute cursor-pointer bottom-2 right-2 bg-[#ac1212] text-white p-2 rounded-full'>Delete</Button>
+          </div>
+          <div className='w-[300px] h-[100px] border rounded-2xl text-center relative mx-auto'>
+            <h3 className='text-xl font-semibold'>Title</h3>
+            <p className='text-sm'>Description</p>
+            <text className='text-sm'>Price</text>
+            <Button className='absolute cursor-pointer top-2 right-2 bg-[#125BAC] text-white p-2 rounded-full'>Edit</Button>
+            <Button className='absolute cursor-pointer bottom-2 right-2 bg-[#ac1212] text-white p-2 rounded-full'>Delete</Button>
+          </div>
+          <div className='w-[300px] h-[100px] border rounded-2xl text-center relative mx-auto'>
+            <h3 className='text-xl font-semibold'>Title</h3>
+            <p className='text-sm'>Description</p>
+            <text className='text-sm'>Price</text>
+            <Button className='absolute cursor-pointer top-2 right-2 bg-[#125BAC] text-white p-2 rounded-full'>Edit</Button>
+            <Button className='absolute cursor-pointer bottom-2 right-2 bg-[#ac1212] text-white p-2 rounded-full'>Delete</Button>
+          </div>
+          <div className='w-[300px] h-[100px] border rounded-2xl text-center relative mx-auto'>
+            <h3 className='text-xl font-semibold'>Title</h3>
+            <p className='text-sm'>Description</p>
+            <text className='text-sm'>Price</text>
+            <Button className='absolute cursor-pointer top-2 right-2 bg-[#125BAC] text-white p-2 rounded-full'>Edit</Button>
+            <Button className='absolute cursor-pointer bottom-2 right-2 bg-[#ac1212] text-white p-2 rounded-full'>Delete</Button>
+          </div>
+          <div className='w-[300px] h-[100px] border rounded-2xl text-center relative mx-auto'>
+            <h3 className='text-xl font-semibold'>Title</h3>
+            <p className='text-sm'>Description</p>
+            <text className='text-sm'>Price</text>
+            <Button className='absolute cursor-pointer top-2 right-2 bg-[#125BAC] text-white p-2 rounded-full'>Edit</Button>
+            <Button className='absolute cursor-pointer bottom-2 right-2 bg-[#ac1212] text-white p-2 rounded-full'>Delete</Button>
+          </div>
+          <div className='w-[300px] h-[100px] border rounded-2xl text-center relative mx-auto'>
+            <h3 className='text-xl font-semibold'>Title</h3>
+            <p className='text-sm'>Description</p>
+            <text className='text-sm'>Price</text>
+            <Button className='absolute cursor-pointer top-2 right-2 bg-[#125BAC] text-white p-2 rounded-full'>Edit</Button>
+            <Button className='absolute cursor-pointer bottom-2 right-2 bg-[#ac1212] text-white p-2 rounded-full'>Delete</Button>
+          </div>
+          <div className='w-[300px] h-[100px] border rounded-2xl text-center relative mx-auto'>
+            <h3 className='text-xl font-semibold'>Title</h3>
+            <p className='text-sm'>Description</p>
+            <text className='text-sm'>Price</text>
+            <Button className='absolute cursor-pointer top-2 right-2 bg-[#125BAC] text-white p-2 rounded-full'>Edit</Button>
+            <Button className='absolute cursor-pointer bottom-2 right-2 bg-[#ac1212] text-white p-2 rounded-full'>Delete</Button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
