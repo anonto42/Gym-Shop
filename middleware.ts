278d114d986c1522e01eaddff9ca5eca31bdf44b
Sohidul Islam Ananto <anontom90@gmail.com>
2025-11-06 10:00:49 +0600

    Middleware configed

--- middleware.ts
+++ middleware.ts
@@ -1,36 +1,41 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-import {getCookie} from "@/server/helper/jwt.helper";
-import {IUser} from "@/server/models/user/user.interfce";
-import {USER_ROLE} from "@/enum/user.enum";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { IUser } from "@/server/models/user/user.interfce";
+import { USER_ROLE } from "@/enum/user.enum";
 
 export async function middleware(request: NextRequest) {
-    // Define your protected routes
-    const protectedRoutesForAdmins = ['/dashboard'];
-    const protectedRoutes = ['/profile'];
+  const path = request.nextUrl.pathname;
+  const protectedRoutes = ["/profile"];
+  const adminRoutes = ["/dashboard"];
 
-    const path = request.nextUrl.pathname;
-
-    // Skip middleware for internal Next.js paths (e.g., _next)
-    if (path.startsWith('/_next')) {
-        return NextResponse.next();
-    }
-
-    const isAuthenticated = await getCookie("user");
-
-    if (!isAuthenticated && protectedRoutes.includes(path)) {
-        const loginUrl = new URL('/auth/signin', request.url);
-        return NextResponse.redirect(loginUrl);
-    }
-
-    if(!isAuthenticated) return;
-
-    const user = JSON.parse(isAuthenticated) as IUser;
-
-    if (user.role != USER_ROLE.ADMIN && protectedRoutesForAdmins.includes(path)) {
-        const returnUrl = new URL('/', request.url);
-        return NextResponse.redirect(returnUrl);
-    }
-
+  if (path.startsWith("/_next") || path.startsWith("/favicon.ico")) {
     return NextResponse.next();
+  }
+
+  const token = request.cookies.get("GYM_Shop")?.value || null;
+  if ((!token || token === "undefined" || token === "null") && (protectedRoutes.includes(path) || adminRoutes.includes(path))) {
+    const loginUrl = new URL("/auth/signin", request.url);
+    return NextResponse.redirect(loginUrl);
+  }
+  if (!token) return NextResponse.next();
+
+  let user: IUser | null = null;
+  try {
+    user = JSON.parse(token);
+  } catch (err) {
+    console.error("Invalid token JSON:", err);
+    const loginUrl = new URL("/auth/signin", request.url);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  if (user?.role !== USER_ROLE.ADMIN && adminRoutes.includes(path)) {
+    const homeUrl = new URL("/", request.url);
+    return NextResponse.redirect(homeUrl);
+  }
+
+  return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/profile/:path*"],
+};
