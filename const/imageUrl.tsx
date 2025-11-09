810cfd79ec736d6acc38e3b1c7748a46b0b28cf9
Sohidul Islam Ananto <anontom90@gmail.com>
2025-11-10 01:39:59 +0600

    const image

---
+++ const/imageUrl.tsx
@@ -0,0 +1,30 @@
+
+
+const imageUrl = {
+
+    heroImage: "https://res.cloudinary.com/ddsnont4o/image/upload/v1760024951/5b86a205885eb07b043f24f68e77fc064d9faa6e_yzxdw6.png",
+
+    aboutServiceImages: {
+        delivery: "https://res.cloudinary.com/ddsnont4o/image/upload/v1760025174/image-from-rawpixel-id-15238167-png_lhjhwm.png",
+        customerCear: "https://res.cloudinary.com/ddsnont4o/image/upload/v1760025269/image-from-rawpixel-id-15039707-png_jwhgde.png",
+        product: "https://res.cloudinary.com/ddsnont4o/image/upload/v1760025348/jhkjjhk_iesrwh.png"
+    },
+
+    product: "https://res.cloudinary.com/ddsnont4o/image/upload/v1760026076/product_bbys1y.png",
+
+    packageImage: {
+        image1: "https://res.cloudinary.com/ddsnont4o/image/upload/v1760026206/package1_k2ljpb.png",
+        image2: "https://res.cloudinary.com/ddsnont4o/image/upload/v1760026265/packa2_pzp8y5.png",
+        image3: "https://res.cloudinary.com/ddsnont4o/image/upload/v1760026312/package3_vwsgds.png"
+    },
+
+    about: {
+        1: "https://res.cloudinary.com/ddsnont4o/image/upload/v1760286143/about1_l9awj1.png",
+        2: "https://res.cloudinary.com/ddsnont4o/image/upload/v1760286203/about2_lvcmri.png",
+        3: "https://res.cloudinary.com/ddsnont4o/image/upload/v1760286319/about3_nz5l4i.png",
+        4: "https://res.cloudinary.com/ddsnont4o/image/upload/v1760286353/about4_ts3dqm.png"
+    }
+
+};
+
+export default imageUrl;
