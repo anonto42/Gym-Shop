2584686a7e5c298d6c2b67f24c741d6a1b394ddd
Sohidul Islam Ananto <anontom90@gmail.com>
2025-11-18 00:44:46 +0600

    added the localhost

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-      domains: ["res.cloudinary.com"],
-      // Or use remotePatterns (recommended):
+      domains: ["res.cloudinary.com","localhost"],
       remotePatterns: [
           {
               protocol: 'https',
