0190316c8472bb5481fc775cd03c35f5beacd4fb
Sohidul Islam Ananto <anontom90@gmail.com>
2025-11-24 10:15:25 +0600

    changed the error log

--- server/db.ts
+++ server/db.ts
@@ -14,7 +14,7 @@ const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
     throw new Error(
-        'Please define the MONGODB_URI environment variable inside .env.local.local'
+        'Please define the MONGODB_URI environment variable inside .env'
     );
 }
 
